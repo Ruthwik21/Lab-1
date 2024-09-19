d6b5e863de1e0fba6bb6d42aa029c2cf2d64238b
Ruthwik21 <rutwikreddy97@gmail.com>
2024-09-19 15:02:10 -0500

    Initial Commit

---
+++ Lab 1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lab 1
+//
+//  Created by Lyle Dev, Loaner on 9/19/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Lab 1Tests/Lab_1Tests.swift
@@ -0,0 +1,17 @@
+//
+//  Lab_1Tests.swift
+//  Lab 1Tests
+//
+//  Created by Lyle Dev, Loaner on 9/19/24.
+//
+
+import Testing
+@testable import Lab_1
+
+struct Lab_1Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
